3ab6b8b0e6e7130b11bf2cec2630783e3ecbe6b9
Manuel Torres Torres <86366833+manutowersdev@users.noreply.github.com>
2021-09-16 01:12:17 +0200

    Update nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -10,3 +10,6 @@ function scrollToNavItem() {
   }
 
   scrollToNavItem();
+const sections = document.querySelectorAll('section'); const div = document.querySelector('#main'); const h1 = document.querySelector('.page-title');
+
+div.innerHTML = ${h1.outerHTML} ${sections[1].outerHTML};
