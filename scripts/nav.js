1f5ee69ac1be660af214372f12fc6ace0bcf4e7f
Manu <manutorres1996@gmail.com>
2021-09-15 15:55:47 +0200

    Updates

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,23 @@
 function scrollToNavItem() {
-    var path = window.location.href.split('/').pop().replace(/\.html/, '');
-    document.querySelectorAll('nav a').forEach(function(link) {
-      var href = link.attributes.href.value.replace(/\.html/, '');
-      if (path === href) {
-        link.scrollIntoView({block: 'center'});
-        return;
-      }
-    })
-  }
+  var path = window.location.href
+    .split('/')
+    .pop()
+    .replace(/\.html/, '');
+  document.querySelectorAll('nav a').forEach(function (link) {
+    var href = link.attributes.href.value.replace(/\.html/, '');
+    if (path === href) {
+      link.scrollIntoView({ block: 'center' });
+      return;
+    }
+  });
+}
 
-  scrollToNavItem();
+scrollToNavItem();
+const sections = document.querySelectorAll('section');
+const div = document.querySelector('#main');
+const h1 = document.querySelector('.page-title');
+
+div.innerHTML = `
+  ${h1.outerHTML}
+  ${sections[1].outerHTML}
+  `;
