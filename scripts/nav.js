0242c21ec7753a411101c8c37bd7bc14be310a2c
Manu <manutorres1996@gmail.com>
2021-09-17 18:11:27 +0200

    Updates

--- scripts/nav.js
+++ scripts/nav.js
@@ -10,6 +10,3 @@ function scrollToNavItem() {
   }
 
   scrollToNavItem();
-const sections = document.querySelectorAll('section'); const div = document.querySelector('#main'); const h1 = document.querySelector('.page-title');
-
-div.innerHTML = ${h1.outerHTML} ${sections[1].outerHTML};
