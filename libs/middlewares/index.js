ed92992953b2eae97711e9bd7ca4e1e4c8d1db3f
Manu <manutorres1996@gmail.com>
2021-09-18 00:01:40 +0200

    canEdit, index(middlewares), deleteProperty, +deleteProperties en deleteUser + JSDOC

---
+++ libs/middlewares/index.js
@@ -0,0 +1,9 @@
+const authUser = require('./authUser');
+const canEdit = require('./canEdit');
+const userExists = require('./userExists');
+
+module.exports = {
+  authUser,
+  canEdit,
+  userExists,
+};
---
+++ models/propertySchema.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+let propertySchema;
+let editPropertySchema;
+module.exports = {
+  propertySchema,
+  editPropertySchema,
+};
+// {
+//   "province": "Joi.string()",
+//   "adress": "Joi.string()",
+//   "zipCode": "Joi.number()",
+//   "number": "Joi.number()",
+//   "stair": "Joi.string()",
+//   "flat": "Joi.number()",
+//   "gate": "Joi.string()",
+//   "mts": "Joi.number()",
+//   "garage": "Joi.boolean()",
+//   "bedrooms": "Joi.number()",
+//   "terrace": "Joi.boolean()",
+//   "toilets": "Joi.number()",
+//   "energyCertificate": "Joi.boolean()",
+//   "price": "Joi.number()",
+//   "city": "Joi.string()",
+//   "estate": "Joi.string()"}
